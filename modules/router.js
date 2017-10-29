83a510171977cb211a48079b440f41cc599521cf
Niels Heltner <niels.heltner@hotmail.com>
2017-10-29 17:09:22 +0100

    mere express-agtig server struktur

--- modules/router.js
+++ modules/router.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-var database = require('./database.js');
 var url = require('url');
-
 
 sendHeader = function(httpCode, response) {
     response.writeHead(httpCode, {
@@ -10,67 +8,6 @@ sendHeader = function(httpCode, response) {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
     });
-}
-
-getUser = function(request, response) {
-    var urlParts = url.parse(request.url, true);
-    database.getUser(urlParts.query, function(userDTO) {
-        response.end(JSON.stringify(userDTO));
-    });
-}
-
-getThread = function(request, response) {
-    var urlParts = url.parse(request.url, true);
-    database.getThread(urlParts.query, function(threadDTO) {
-        response.end(JSON.stringify(threadDTO));
-    });
-}
-
-getThreadComments = function(request, response) {
-    var urlParts = url.parse(request.url, true);
-    database.getThreadComments(urlParts.query, function(commentsDTO) {
-        response.end(JSON.stringify(commentsDTO));
-    });
-}
-
-getCategories = function(request, response) {
-    database.getCategories(function(categoriesDTO) {
-        response.end(JSON.stringify(categoriesDTO));
-    });
-}
-
-getThreadsInCategory = function(request, response) {
-    var urlParts = url.parse(request.url, true);
-    database.getThreadsInCategory(urlParts.query, function(threadsDTO) {
-        response.end(JSON.stringify(threadsDTO));
-    });
-}
-
-getUserThreads = function(request, response) {
-    var urlParts = url.parse(request.url, true);
-    database.getUserThreads(urlParts.query, function(threadsDTO) {
-        response.end(JSON.stringify(threadsDTO));
-    });
-}
-
-putUser = function(request, response) { //create
-    request.on('data', function(data) {
-        console.log('Received user creation request for: ' + data);
-        database.saveUser(data, function(saveUserDTO) {
-            console.log(saveUserDTO);
-            response.end(JSON.stringify(saveUserDTO));
-        });
-    });
-}
-
-postUser = function(request, response) { //login
-    request.on('data', function(data) {
-        console.log('Received login request for: ' + data);
-        database.logIn(data, function(logInDTO) {
-            console.log(logInDTO);
-            response.end(JSON.stringify(logInDTO));
-        });
-    })
 }
 
 handler_delete = function(request, response) {
@@ -87,15 +24,6 @@ sendOptions = function(request, response) {
 }
 
 routes = {
-    'GET/users': getUser,
-    'GET/thread': getThread,
-    'GET/thread/comments': getThreadComments,
-    'GET/categories': getCategories,
-    'GET/categories/threads': getThreadsInCategory,
-    'GET/users/threads': getUserThreads,
-    'PUT/users': putUser,
-    'POST/users': postUser,
-    'DELETE': handler_delete,
     'OPTIONS': sendOptions
 }
 
@@ -115,5 +43,22 @@ module.exports = {
             sendHeader(404, response);
             response.end(null); // egentligt burde statussen fra senderHeader sættes til 404 i stedet for 200 her
         }
+    },
+
+    get: function(path, func) {
+        routes['GET' + path] = func;
+    },
+
+    put: function(path, func) {
+        routes['PUT' + path] = func;
+    },
+
+    post: function(path, func) {
+        routes['POST' + path] = func;
+    },
+
+    delete: function(path, func) {
+        routes['DELETE' + path] = func;
     }
+
 }
